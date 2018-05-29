9c02a48043440e928fd6ebddbbc5aa2cb98267ec
NeverMore93 <1305882275@qq.com>
2018-05-29 18:10:26 +0800

    Organize the code

--- src/main/java/com/ywt/eureka_client/controller/ServiceInstanceRestController.java
+++ src/main/java/com/ywt/eureka_client/controller/ServiceInstanceRestController.java
@@ -3,6 +3,7 @@ package com.ywt.eureka_client.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,5 +18,10 @@ public class ServiceInstanceRestController {
     @RequestMapping("/service-instances/{applicationName}")
     public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
         return this.discoveryClient.getInstances(applicationName);
+    }
+
+    @GetMapping(name = "/greeting")
+    public String greeting() {
+        return "Hello from EurekaClient!";
     }
 }
